2ef3f46f8464bb0a662e984c518389219fd460c8
atlauren <atlauren@uci.edu>
2024-01-30 22:32:48 -0800

    Ready for testing

--- correct-onedrive-forbidden.sh
+++ correct-onedrive-forbidden.sh
@@ -1,31 +1,63 @@
 #!/bin/zsh
 # https://github.com/atlauren/OneDriveKFM/
 # atlauren@uci.edu
+# 
 # https://support.microsoft.com/en-us/office/restrictions-and-limitations-in-onedrive-and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa
+# https://zsh.sourceforge.io/Doc/Release/zsh_toc.html
+# https://zsh.sourceforge.io/Doc/Release/Expansion.html
+# https://github.com/zsh-users/zsh/blob/master/Functions/Misc/zmv
+# https://stackoverflow.com/a/41508466/20266908
+
+theRoot=$HOME/Desktop/
+theName=OneDrive-Renamed
+theOutput=$theName.txt
+theCSV=$theName.csv
 
 autoload zmv
 
-# substitute leading spaces with "_"
-zmv -n '(**/) (*)' '$1_$2'
+zmv $theRoot'(OneDrive-Renamed).(txt|csv)(#qN)' $theRoot'$1_$(stat -f %Sc -t %F_%H-%M-%S $f).$2'
 
-# substitute trailing spaces with "_"
-zmv -n '(**/)(*) ' '$1$2_'
+#rm $theRoot$theOutput
+touch $theRoot$theOutput
 
-# substitute illegal characters with "_"
-# " * : < > ? / \ | %
-# https://unix.stackexchange.com/a/767152/599230
-zmv -n '(**/)(*)' '$1${(S)2//([\\:\|><\?\*\%"\""])/_}'
+for q in / . ; do
+# (#q/) directories
+# (#q.) files
 
-# prepend with "_"
-# CON PRN AUX NUL
-zmv -n '(**/)(AUX|CON|NUL|PRN)' '$1_$2'
+	for i in Desktop/ Documents/ ; do
+	
+		# leading spaces -- substitute with "_"
+		zmv -n $HOME/$i'(**/) (*)(#q'$q')(#qN)' $HOME/$i'$1_$2' >> $theRoot$theOutput
+		
+		# trailing spaces -- substitute with "_"
+		zmv -n $HOME/$i'(**/)(*) (#q'$q')(#qN)' $HOME/$i'$1$2_' >> $theRoot$theOutput
+		
+		# illegal characters -- substitute with "_"
+		# " * : < > ? / \ | %
+		# https://unix.stackexchange.com/a/767152/599230
+		zmv -n $HOME/$i'(**/)(*)(#q'$q')(#qN)' $HOME/$i'$1${(S)2//([\\:\|><\?\*\%"\""])/_}' >> $theRoot$theOutput
+		
+		# CON PRN AUX NUL -- prepend with "_"
+		zmv -n $HOME/$i'(**/)(AUX|CON|NUL|PRN)(#q'$q')(#qN)' $HOME/$i'$1_$2' >> $theRoot$theOutput
+		
+		# COM[0-9], LPT[0-9] -- prepend with "_"
+		zmv -n $HOME/$i'(**/)(COM|LPT)([0-9])(#q'$q')(#qN)' $HOME/$i'$1_$2$3' >> $theRoot$theOutput
+	
+		# _vti_ -- remove "_"
+		zmv -n $HOME/$i'(**/)_(vti)_(#q'$q')(#qN)' $HOME/$i'$1$2' >> $theRoot$theOutput
+		
+		# .lock -- replace . with "_"
+		zmv -n $HOME/$i'(**/).(lock)(#q'$q')(#qN)' $HOME/$i'$1_$2' >> $theRoot$theOutput
+	
+		# desktop.ini -- replace . with "_"
+		zmv -n $HOME/$i'(**/)(desktop).(ini)(#q'$q')(#qN)' $HOME/$i'$1$2_$3' >> $theRoot$theOutput
+		
+		# files begining ~$ -- replace ~$ with "__"
+		zmv -n $HOME/$i'(**/)\~$(*)(#q.)(#qN)' $HOME/$i'$1__$2' >> $theRoot$theOutput	
+	done
 
-# prepend with "_"
-# COM0 - COM9
-# LPT0 - LPT9
-zmv -n '(**/)(COM|LPT)([0-9])' '$1_$2$3'
+done
 
-# remove "_"
-# _vti_
-zmv -n '(**/)_(vti)_' '$1$2'
+awk -f ./zmv2csv.awk $theRoot$theOutput > $theRoot$theCSV
 
+# end
---
+++ zmv2csv.awk
@@ -0,0 +1,30 @@
+BEGIN {
+print "\042BEFORE\042,\042AFTER\042"
+}
+{
+# if not line ending ' append ' to end of line
+if (!/\x27$/)
+	{ sub(/$/,"\047")
+	}
+# add ' to end of "mv -- /"
+if (/^mv -- \//)
+	{sub(/\//,"\047\/")
+	}
+# add ' to start of " /Users"
+if (/ \/Users/ && !/\x27 \/Users/)
+	{ sub(/ \/Users/,"\047 \/Users")
+	}
+# add ' to start of " /Users"
+if (/ \/Users/)
+	{ sub(/ \/Users/," \047\/Users")
+	}
+# trim "mv -- " from start of lines
+sub (/^mv -- /,"")
+# change ' ' to ','
+sub(/\x27 \x27/,"\047,\047")
+# replace all " with ""
+gsub(/\x22/,"\042\042")
+# replace all ' with "
+gsub (/\x27/,"\042")
+print $0
+}
