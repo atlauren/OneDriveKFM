d100a22a3082b991597e1e6b73919dab01ab023d
atlauren <atlauren@uci.edu>
2024-02-08 16:01:08 -0800

    Verbose output!

--- correct-onedrive-forbidden.sh
+++ correct-onedrive-forbidden.sh
@@ -28,33 +28,33 @@ for q in / . ; do
 	for i in Desktop/ Documents/ ; do
 	
 		# leading spaces -- substitute with "_"
-		zmv $theHome/$i'(**/) (*)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/) (*)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 		
 		# trailing spaces -- substitute with "_"
-		zmv $theHome/$i'(**/)(*) (#q'$q')(#qN)' $theHome/$i'$1$2_' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)(*) (#q'$q')(#qN)' $theHome/$i'$1$2_' >> $theRoot$theOutput
 		
 		# illegal characters -- substitute with "_"
 		# " * : < > ? / \ | %
 		# https://unix.stackexchange.com/a/767152/599230
-		zmv $theHome/$i'(**/)(*)(#q'$q')(#qN)' $theHome/$i'$1${(S)2//([\\:\|><\?\*\%"\""])/_}' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)(*)(#q'$q')(#qN)' $theHome/$i'$1${(S)2//([\\:\|><\?\*\%"\""])/_}' >> $theRoot$theOutput
 		
 		# CON PRN AUX NUL -- prepend with "_"
-		zmv $theHome/$i'(**/)(AUX|CON|NUL|PRN)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)(AUX|CON|NUL|PRN)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 		
 		# COM[0-9], LPT[0-9] -- prepend with "_"
-		zmv $theHome/$i'(**/)(COM|LPT)([0-9])(#q'$q')(#qN)' $theHome/$i'$1_$2$3' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)(COM|LPT)([0-9])(#q'$q')(#qN)' $theHome/$i'$1_$2$3' >> $theRoot$theOutput
 	
 		# _vti_ -- remove "_"
-		zmv $theHome/$i'(**/)_(vti)_(#q'$q')(#qN)' $theHome/$i'$1$2' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)_(vti)_(#q'$q')(#qN)' $theHome/$i'$1$2' >> $theRoot$theOutput
 		
 		# .lock -- replace . with "_"
-		zmv $theHome/$i'(**/).(lock)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/).(lock)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 	
 		# desktop.ini -- replace . with "_"
-		zmv $theHome/$i'(**/)(desktop).(ini)(#q'$q')(#qN)' $theHome/$i'$1$2_$3' >> $theRoot$theOutput
+		zmv -v $theHome/$i'(**/)(desktop).(ini)(#q'$q')(#qN)' $theHome/$i'$1$2_$3' >> $theRoot$theOutput
 		
 		# files begining ~$ -- replace ~$ with "__"
-		zmv $theHome/$i'(**/)\~$(*)(#q.)(#qN)' $theHome/$i'$1__$2' >> $theRoot$theOutput	
+		zmv -v $theHome/$i'(**/)\~$(*)(#q.)(#qN)' $theHome/$i'$1__$2' >> $theRoot$theOutput	
 	done
 
 done
