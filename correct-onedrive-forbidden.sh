bfa01e45f29462248f64c61f43931d0b1791ecf4
atlauren <atlauren@uci.edu>
2024-02-12 21:51:42 -0800

    Add, enforce --dry-run and --commit flags.

--- correct-onedrive-forbidden.sh
+++ correct-onedrive-forbidden.sh
@@ -9,6 +9,26 @@
 # https://github.com/zsh-users/zsh/blob/master/Functions/Misc/zmv
 # https://stackoverflow.com/a/41508466/20266908
 
+if (( # == 0 )) ; then
+	echo "usage: correct-onedrive-forbidden.sh <cmd>"
+	echo ""
+	echo "  --dry-run    Report only; make no changes."
+	echo "  --commit     Write changes to files."
+	echo ""
+	exit 0
+fi
+
+if [[ ${1} = "--dry-run" ]] ; then
+	theFlag="n"     # do Nothing
+	theTouch="no-go"
+elif [[ ${1} = "--commit" ]] ; then
+	theFlag="v"     # output Verbose
+	theTouch="go"
+else 
+	echo "Should not land here."
+	exit 1
+fi
+
 theHome=$(dscl . read /Users/$USER NFSHomeDirectory | awk '{print $2}')
 theRoot=$theHome/Desktop/
 theName=OneDrive-Renamed
@@ -28,33 +48,33 @@ for q in / . ; do
 	for i in Desktop/ Documents/ ; do
 	
 		# leading spaces -- substitute with "_"
-		zmv -v $theHome/$i'(**/) (*)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/) (*)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 		
 		# trailing spaces -- substitute with "_"
-		zmv -v $theHome/$i'(**/)(*) (#q'$q')(#qN)' $theHome/$i'$1$2_' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)(*) (#q'$q')(#qN)' $theHome/$i'$1$2_' >> $theRoot$theOutput
 		
 		# illegal characters -- substitute with "_"
 		# " * : < > ? / \ | %
 		# https://unix.stackexchange.com/a/767152/599230
-		zmv -v $theHome/$i'(**/)(*)(#q'$q')(#qN)' $theHome/$i'$1${(S)2//([\\:\|><\?\*\%"\""])/_}' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)(*)(#q'$q')(#qN)' $theHome/$i'$1${(S)2//([\\:\|><\?\*\%"\""])/_}' >> $theRoot$theOutput
 		
 		# CON PRN AUX NUL -- prepend with "_"
-		zmv -v $theHome/$i'(**/)(AUX|CON|NUL|PRN)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)(AUX|CON|NUL|PRN)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 		
 		# COM[0-9], LPT[0-9] -- prepend with "_"
-		zmv -v $theHome/$i'(**/)(COM|LPT)([0-9])(#q'$q')(#qN)' $theHome/$i'$1_$2$3' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)(COM|LPT)([0-9])(#q'$q')(#qN)' $theHome/$i'$1_$2$3' >> $theRoot$theOutput
 	
 		# _vti_ -- remove "_"
-		zmv -v $theHome/$i'(**/)_(vti)_(#q'$q')(#qN)' $theHome/$i'$1$2' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)_(vti)_(#q'$q')(#qN)' $theHome/$i'$1$2' >> $theRoot$theOutput
 		
 		# .lock -- replace . with "_"
-		zmv -v $theHome/$i'(**/).(lock)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/).(lock)(#q'$q')(#qN)' $theHome/$i'$1_$2' >> $theRoot$theOutput
 	
 		# desktop.ini -- replace . with "_"
-		zmv -v $theHome/$i'(**/)(desktop).(ini)(#q'$q')(#qN)' $theHome/$i'$1$2_$3' >> $theRoot$theOutput
+		zmv -$theFlag $theHome/$i'(**/)(desktop).(ini)(#q'$q')(#qN)' $theHome/$i'$1$2_$3' >> $theRoot$theOutput
 		
 		# files begining ~$ -- replace ~$ with "__"
-		zmv -v $theHome/$i'(**/)\~$(*)(#q.)(#qN)' $theHome/$i'$1__$2' >> $theRoot$theOutput	
+		zmv -$theFlag $theHome/$i'(**/)\~$(*)(#q.)(#qN)' $theHome/$i'$1__$2' >> $theRoot$theOutput	
 	done
 
 done
@@ -74,10 +94,9 @@ for f in $theFiles
 do
 	echo "$f" >> $theHome/Desktop/OneDrive-FileDates.txt
  	theTime=$(date -r "$f" +%H%M.%S)
- 	echo $theTime
- 	# stat "$f"
- 	touch -t 19800101$theTime "$f"
- 	# stat "$f" 	
+ 	if [[ $theTouch = "go"]] ; then
+ 		touch -t 19800101$theTime "$f"
+ 	fi
 done
 
 
